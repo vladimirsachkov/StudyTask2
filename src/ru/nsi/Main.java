45116e59a126ff9c176f9a180243b80bdd5c725d
alexa <alexanderredar1@gmail.com>
2021-11-17 06:19:35 +0300

    1) Добавлен главный метод, который должен будет генерировать простые числа в заданных пользователем диапазоне. 2) Добавлен метод isPrimeNumber, который будет проверять является ли входящее число простым или нет.

--- src/ru/nsi/Main.java
+++ src/ru/nsi/Main.java
@@ -1,4 +1,42 @@
 package ru.nsi;
 
 public class Main {
+
+    private boolean isPrimeNumber (int number) {
+        boolean isPrimeNumber = false;
+        int numberOfDivision = 0;
+        if (number > 1) {
+            int mod;
+            for (int i = 2; i<number; i++) {
+              mod = number % i;
+              if (mod == 0) {
+                  numberOfDivision = numberOfDivision + 1;
+              }
+            }
+        }
+        if( numberOfDivision == 0) {
+            return isPrimeNumber;
+        }
+        else {
+            isPrimeNumber = true;
+            return isPrimeNumber;
+        }
+    }
+
+    public static void main(String[] args) {
+        int begin = Integer.parseInt(args[0]);
+        int end = Integer.parseInt(args[1]);
+
+        for (int i = begin; i<=begin; i++) {
+            System.out.print(1);
+        };
+
+//        for (int i = begin; i <= end; i++) {
+//            System.out.print(i + " ");
+//        }
+//
+//        for (int i = begin; i <= end; i++) {
+//
+//        }
+    }
 }
