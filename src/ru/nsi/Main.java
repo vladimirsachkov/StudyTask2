acc0fa89e7838dce89086a60f7e0dffd7ebf7b87
alexa <alexanderredar1@gmail.com>
2021-11-18 05:34:38 +0300

    1) Доработан метод isPrimeNumber, который осуществляет проверку, является ли входное число простым или нет. При положительном результате метод возвращает true, при отрицательном - false. 2) Добавлен главный метод, который принимает 2 параметра: начало интервала и конец интервала. Из полученного интервала выводятся только те числа, которые являются простыми.

--- src/ru/nsi/Main.java
+++ src/ru/nsi/Main.java
@@ -2,41 +2,38 @@ package ru.nsi;
 
 public class Main {
 
-    private boolean isPrimeNumber (int number) {
+    private static boolean isPrimeNumber(int number) {
         boolean isPrimeNumber = false;
         int numberOfDivision = 0;
-        if (number > 1) {
+        if (number == 0 || number == 1) {
+            return isPrimeNumber;
+        } else {
             int mod;
-            for (int i = 2; i<number; i++) {
-              mod = number % i;
-              if (mod == 0) {
-                  numberOfDivision = numberOfDivision + 1;
-              }
+            for (int i = 2; i < number; i++) {
+                mod = number % i;
+                if (mod == 0) {
+                    numberOfDivision = numberOfDivision + 1;
+                }
             }
-        }
-        if( numberOfDivision == 0) {
-            return isPrimeNumber;
-        }
-        else {
-            isPrimeNumber = true;
-            return isPrimeNumber;
+            if (numberOfDivision > 0) {
+                return isPrimeNumber;
+            } else {
+                isPrimeNumber = true;
+                return isPrimeNumber;
+            }
         }
     }
 
     public static void main(String[] args) {
         int begin = Integer.parseInt(args[0]);
         int end = Integer.parseInt(args[1]);
-
-        for (int i = begin; i<=begin; i++) {
-            System.out.print(1);
-        };
-
-//        for (int i = begin; i <= end; i++) {
-//            System.out.print(i + " ");
-//        }
-//
-//        for (int i = begin; i <= end; i++) {
-//
-//        }
+        System.out.print("BEGIN: " + begin + ", END: " + end);
+        System.out.println();
+        System.out.print("PRIME NUMBERS: ");
+        for (int i = begin; i <= end; i++) {
+            if (isPrimeNumber(i) == true) {
+                System.out.print(i + " ");
+            }
+        }
     }
 }
